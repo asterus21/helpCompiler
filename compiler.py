a7377d01d5f93146ea3353a226410f9dc08e715c
Mikhail Palvov <mpavlov@megaputer.ru>
2024-02-13 10:34:03 +0300

    "New line separator fixed"

--- compiler.py
+++ compiler.py
@@ -25,7 +25,7 @@ class HelpCompiler:
         as a string to execute a Command Line (CMD) script."""
 
         out = subprocess.run(
-            f"@ECHO OFF && pushd D:\\pa_config && set COMLICBITSPATH=D:\\pa_config\\comlicbits.h && pushd C:\\pa6autotests\\Builds\\{build_number}\\SourceData && set SOURCEDATA=C:\\pa6autotests\\Builds\\{build_number}\\SourceData && pushd D:\\gitbash\\help && build.cmd", shell=True
+            f"@ECHO OFF&&pushd D:\\pa_config&&set COMLICBITSPATH=D:\\pa_config\\comlicbits.h&&pushd C:\\pa6autotests\\Builds\\{build_number}\\SourceData&&set SOURCEDATA=C:\\pa6autotests\\Builds\\{build_number}\\SourceData&&pushd D:\\gitbash\\help&&D:\\gitbash\\help\\build.cmd", shell=True
             )
 
         return out
