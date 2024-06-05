003e6d0ac820c4ca5e3f62fac43ecaa028e1b32c
Mikhail Palvov <mpavlov@megaputer.ru>
2024-06-05 16:56:13 +0300

    Check if the folder exists and not empty added

--- compiler.py
+++ compiler.py
@@ -1,36 +1,57 @@
+"""Module to compile a new version of the Help"""
+
 import os
 import subprocess
+from icecream import ic
 
+BUILD_PATH = "C:/pa6autotests/Builds"
 
-class HelpCompiler:
+# check to see if the folder exists
+def if_folder(build_path: str) -> bool:
 
-    """Class to include functions to compile
-    the current version of the User Manual."""
+    """The function checks if the folder with builds exists."""
 
-    def __init__(self) -> None:
-        pass
+    if os.path.exists(build_path):
+        ic('pass')
+    else:
+        raise ValueError('The folder does not exist.')
 
-# it is needed to get the build number in a different way
+# getting a list of subfolders and files
+def make_list(build_path: str) -> list:
 
-    def show_current_build(self, build_path: str) -> str:
+    """The function accepts a path to the build
+    and returns a list of subfolders."""
 
-        """The function is to accept a path to an auto-test build
-        and returns a first (usually a single item) of the list."""
+    list_folders = os.listdir(build_path)
+    return list_folders
 
-        build_number = os.listdir(path=build_path)[0]
-        
-        return build_number
-    
-# it is needed to check if the folder exists
-# otherwise, raise an error
+# check to see if the lisf of subfolders and files is empty
+def if_empty(list_folders: list) -> bool:
 
-    def excecute_script(self, build_number: str) -> bytes:
+    """The function checks whether the list is empty."""
 
-        """The function is to accept a build number
-        as a string to execute a Command Line (CMD) script."""
+    if list_folders:
+        ic('pass')
+    else:
+        raise ValueError('The folder is empty.')
 
-        out = subprocess.run(
-            f"@ECHO OFF&&pushd D:\\pa_config&&set COMLICBITSPATH=D:\\pa_config\\comlicbits.h&&pushd C:\\pa6autotests\\Builds\\{build_number}\\SourceData&&set SOURCEDATA=C:\\pa6autotests\\Builds\\{build_number}\\SourceData&&pushd D:\\gitbash\\help&&D:\\gitbash\\help\\build.cmd", shell=True
-            )
+# getting the first item of the list of subfolders and files
+def show_current_build(list_folders: list) -> str:
 
-        return out
+    """The function is to accept a path to an auto-test build
+    and returns a first (usually a single item) of the list."""
+
+    build_number = list_folders[0]
+    ic(build_number)
+    return build_number
+
+# script to build a new version of the manual
+def excecute_script(build_number: str) -> bytes:
+
+    """The function is to accept a build number
+    as a string to execute a Command Line (CMD) script."""
+
+    out = subprocess.run(
+        f"@ECHO OFF&&pushd D:\\pa_config&&set COMLICBITSPATH=D:\\pa_config\\comlicbits.h&&pushd C:\\pa6autotests\\Builds\\{build_number}\\SourceData&&set SOURCEDATA=C:\\pa6autotests\\Builds\\{build_number}\\SourceData&&pushd D:\\gitbash\\help&&D:\\gitbash\\help\\build.cmd", shell=True, check=False)
+
+    return out
