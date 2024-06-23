564641060ca689fb87e2bbddb1b1c2b2c3797f26
Mikhail Palvov <mpavlov@megaputer.ru>
2024-06-23 14:58:18 +0300

    Refactoring

--- compiler.py
+++ compiler.py
@@ -2,7 +2,6 @@
 
 import os
 import subprocess
-from icecream import ic
 
 BUILD_PATH = "C:/pa6autotests/Builds"
 
@@ -11,10 +10,7 @@ def if_folder(build_path: str) -> bool:
 
     """The function checks if the folder with builds exists."""
 
-    if os.path.exists(build_path):
-        ic('pass')
-    else:
-        raise ValueError('The folder does not exist.')
+    assert os.path.exists(build_path), 'Folder does not exist.'
 
 # getting a list of subfolders and files
 def make_list(build_path: str) -> list:
@@ -22,27 +18,23 @@ def make_list(build_path: str) -> list:
     """The function accepts a path to the build
     and returns a list of subfolders."""
 
-    list_folders = os.listdir(build_path)
-    return list_folders
+    folders = next(os.walk(build_path))[1]
+    return folders
 
 # check to see if the lisf of subfolders and files is empty
-def if_empty(list_folders: list) -> bool:
+def if_empty(folders: list) -> bool:
 
     """The function checks whether the list is empty."""
 
-    if list_folders:
-        ic('pass')
-    else:
-        raise ValueError('The folder is empty.')
+    assert folders, 'The list of folders is empty.'
 
 # getting the first item of the list of subfolders and files
-def show_current_build(list_folders: list) -> str:
+def show_current_build(folders: list) -> str:
 
     """The function is to accept a path to an auto-test build
     and returns a first (usually a single item) of the list."""
 
-    build_number = list_folders[0]
-    ic(build_number)
+    build_number = folders[0]
     return build_number
 
 # script to build a new version of the manual
