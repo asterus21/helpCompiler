bbe6a619e039913255a04f152ea49e3f161dcbb2
Mikhail Palvov <mpavlov@megaputer.ru>
2024-08-09 15:41:48 +0300

    Added files list as constant value

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
-"""The module starts the script to build a new version of the User's Manual"""
+"""Main module"""
 
 import subprocess
 import paths
 
-build_path = paths.file_open(paths.FILE_PATH)[0]
-build_header = paths.file_open(paths.FILE_PATH)[1]
-build_script = paths.file_open(paths.FILE_PATH)[2]
+FILE_PATH = "C:\\variables.txt"
 
-# print(
-#     build_path,
-#     build_header,
-#     build_script
-# )
+buil = paths.file_open(FILE_PATH)[0]
+conf = paths.file_open(FILE_PATH)[1]
+scrp = paths.file_open(FILE_PATH)[2]
 
-# -------------------------------------------------------------------
-# script to build a new version of the manual for the constant values
-# -------------------------------------------------------------------
-def excecute_script_constant_path(build_path: str) -> bytes:
+# the list is considered to be on the 'C:' volume and have the '.txt' format
+
+def excecute_script_constant_path(bld: str, cnf: str, spt: str) -> bytes:
 
     """The function is to accept a build number
     as a string to execute a Command Line (CMD) script."""
 
-
-    out = subprocess.run(f"@ECHO OFF&&pushd D:\\pa_config&&set COMLICBITSPATH=D:\\pa_config\\comlicbits.h&&pushd C:\\pa6autotests\\Builds\\{build_path}\\SourceData&&set SOURCEDATA=C:\\pa6autotests\\Builds\\{build_path}\\SourceData&&pushd D:\\gitbash\\help&&D:\\gitbash\\help\\build.cmd", shell=True, check=False)
+    out = subprocess.run(f"@ECHO OFF&&pushd {cnf}&&set COMLICBITSPATH={cnf}\\comlicbits.h&&pushd {bld}\\SourceData&&set SOURCEDATA={bld}\\SourceData&&pushd {spt}&&{spt}\\build.cmd", shell=True, check=False)
 
     return out
+
+if __name__ == "__main__":
+    excecute_script_constant_path(buil, conf, scrp)
--- paths.py
+++ paths.py
@@ -2,10 +2,6 @@
 
 import os
 
-KEYS = ["BUILD", "HEADER", "SCRIPT"]
-FILE_PATH = "C:\\variables.txt"
-# the second list where paths are stored is considered to be at 'C:' and have the .txt format
-
 # -------------------------------------
 # check the file exists
 # -------------------------------------
@@ -25,81 +21,51 @@ def file_open(file: bytes) -> list:
     with open(file, "r", encoding="utf-8") as f:
         content = f.read().splitlines()
 
-    # print(content)
     return content
-
-# -------------------------------------
-# turn a list into a dictionary
-# -------------------------------------
-def list_to_dict(content: list) -> dict:
-
-    """The function turn a list into a dictionary"""
-
-    dict_ = dict(zip(KEYS, content))
-    # print(dict_)
-    return dict_
 
 # -------------------------------------
 # check to see if the folder exists
 # -------------------------------------
-def if_folder(dict_: dict) -> bool:
+def if_folder(content: list) -> bool:
 
-    """The function checks if the folder with the PA build exists."""
+    """The function checks if the folders exist."""
 
-    for value in dict_.values():
-        # print(value)
+    for value in content:
         assert os.path.exists(value), "Folder does not exist."
-
-# -------------------------------------
-# get a list of folders
-# -------------------------------------
-def get_paths(dict_: dict) -> list:
-
-    """The function returns a list of paths."""
-
-    paths = []
-    for value in dict_.values():
-        paths.append(value)
-
-    # print(paths[0])
-    return paths
 
 # -------------------------------------
 # get a list of files within the folder
 # -------------------------------------
-def list_of_files(paths: list) -> list:
+def list_of_files(content: list) -> list:
 
     """The function returns the list of files for a given path."""
 
     files = []
-    for path in paths:
+    for path in content:
         files.append(os.listdir(path))
 
-    # print(files)
     return files
 
 # -------------------------------------
 # check if a header file exists
 # -------------------------------------
-def if_header(files: list) -> None:
+def if_header(files: list) -> bool:
 
     """The function checks that the file exists within a specified folder."""
 
     if 'comlicbits.h' in files[1]:
-        print("'comlicbits.h' has been found")
         return True
 
-    return None
+    return True
 
 # -------------------------------------
 # check if a script file exists
 # -------------------------------------
-def if_script(files: list) -> None:
+def if_script(files: list) -> bool:
 
     """The function checks that the file exists within a specified folder."""
 
     if 'build.cmd' in files[2]:
-        print("'build.cmd' has been found")
         return True
 
-    return None
+    return True
